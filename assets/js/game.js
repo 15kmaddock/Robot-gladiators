3c96e859bbbcd73d07f6a07e2118919d668a1a69
kody maddock <kodymaddock15k@hotmail.com>
2022-04-18 01:12:35 -0600

    fight function 3.1.6

---
+++ assets/js/game.js
@@ -0,0 +1,37 @@
+var playerName = window.prompt("What is your robot's name?");
+
+var playerHealth = 100;
+
+var playerAttack = 10;
+
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+
+var enemyHealth = 50;
+
+var enemyAttack = 12;
+
+var fight = function() {
+    // Aler players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    //Subtract the value of playerAttack from the value of enemyHealth and use that result to update the value in the enemyHealth variable
+    enemyHealth = enemyHealth - playerAttack;
+
+    // Log a resulting message to the console so we know that is worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    // Subract the value of enemyAttack form the value of playerHealth and use that result to update the value in the playerHealth variable.
+    playerHealth = playerHealth - enemyAttack;
+
+    //Log a resulting message to the console so we know that is worked.
+    console.log(
+        enemyName + " Attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health reamining. "
+    );
+};
+
+fight();
